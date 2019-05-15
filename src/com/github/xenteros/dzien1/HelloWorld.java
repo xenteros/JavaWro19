2d6cbb90543d6621ea5220af876c85326dcc1385
xenteros <adam@gurgul.com.pl>
2019-05-15 21:00:15 +0200

    Initial commit

---
+++ src/com/github/xenteros/dzien1/HelloWorld.java
@@ -0,0 +1,26 @@
+package com.github.xenteros.dzien1;
+
+
+import java.util.Arrays;
+
+class HelloWorld {
+
+    public static void main(String[] args) {
+//        System.out.println(args.length);
+//        System.out.println(Arrays.toString(args));
+//        System.out.println("Hello " + args[0]);
+//        DEKLARACJA Z INICJALIZACJĄ
+        String helloWorld = "Hello World";
+//        DEKLARACJA
+        String byeByeWorld;
+//        INICJALIZACJA
+        byeByeWorld = "Bye Bye World";
+        System.out.println(byeByeWorld);
+        System.out.println(helloWorld);
+//        ZMIANA WARTOŚCI
+        helloWorld = "Hello World!";
+        System.out.println(helloWorld);
+
+
+    }
+}
---
+++ src/com/github/xenteros/dzien1/Primitives.java
@@ -0,0 +1,20 @@
+package com.github.xenteros.dzien1;
+
+class Primitives {
+
+    public static void main(String[] args) {
+        int i = 100000;
+        long number = 2200000L;
+        float twoAndAHalf = 2.5f;
+        double  aHalf = 0.5;
+        char a = 'a';
+        boolean t = true;
+        boolean f = false;
+
+//      operatory   + - * /
+
+        System.out.println(i + number);
+
+    }
+
+}
