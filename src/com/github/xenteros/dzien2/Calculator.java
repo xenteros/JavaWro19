3994b53dc4f3d71c8d75ebf5d3ae6ba737dd5063
xenteros <adam@gurgul.com.pl>
2019-05-16 20:23:14 +0200

    Kody źródłe z zajęć

--- src/com/github/xenteros/dzien2/Calculator.java
+++ src/com/github/xenteros/dzien2/Calculator.java
@@ -7,27 +7,31 @@ class Calculator {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String operator = scanner.nextLine();
-        String firstNumber = scanner.nextLine();
-        String secondNumber = scanner.nextLine();
 
-        // TODO: 16.05.2019 Zamienić liczby ze stringów na inty lub double
-        int a = Integer.parseInt(firstNumber);
-        int b = Integer.parseInt(secondNumber);
-        // TODO: 16.05.2019 Sprawdzić co to za operator, +-* czy / i wyświetlić odpowiedni wynik
+        while (true) {
+            System.out.println("Podaj operator:");
+            String operator = scanner.nextLine();
+            System.out.println("Podaj pierwszą liczbę:");
+            String firstNumber = scanner.nextLine();
+            System.out.println("Podaj drugą liczbę:");
+            String secondNumber = scanner.nextLine();
 
-        if (operator.equals("+")) {
-            System.out.println(a+b);
-        } else if (operator.equals("-")) {
-            System.out.println(a-b);
-        } else if (operator.equals("*")) {
-            System.out.println(a*b);
-        } else if (operator.equals("/")) {
-            System.out.println(a/b);
-        } else {
-            System.out.println("Nie znam tego operatora.");
+            // TODO: 16.05.2019 Zamienić liczby ze stringów na inty lub double
+            int a = Integer.parseInt(firstNumber);
+            int b = Integer.parseInt(secondNumber);
+            // TODO: 16.05.2019 Sprawdzić co to za operator, +-* czy / i wyświetlić odpowiedni wynik
+
+            if (operator.equals("+")) {
+                System.out.println(a + b);
+            } else if (operator.equals("-")) {
+                System.out.println(a - b);
+            } else if (operator.equals("*")) {
+                System.out.println(a * b);
+            } else if (operator.equals("/")) {
+                System.out.println(a / b);
+            } else {
+                System.exit(0);
+            }
         }
-
-
     }
 }
---
+++ src/com/github/xenteros/dzien2/Loops.java
@@ -0,0 +1,21 @@
+package com.github.xenteros.dzien2;
+
+import java.util.Scanner;
+
+class Loops {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+//        String line = scanner.nextLine();
+//
+//        while (line.endsWith("ski")) {
+//            System.out.println(line);
+//            line = scanner.nextLine();
+//        }
+
+//        for (int i = 0; i < n; i++) {
+//            System.out.println(i);
+//        }
+    }
+}
